f5e3fbf384cc6e9af4b3e0c8da0574f8017f28e1
ankush <mehraankush36@gmail.com>
2023-01-29 09:10:33 +0530

    Frequency of numbers in an sorted array

---
+++ Array/FrequencyOfElement/SortedArray.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+void frequency(int arr[],int n){
+    int freq = 1;
+    int i=1;
+    while(i<n){
+        while(i<n && arr[i]==arr[i-1]){
+            freq++;
+            i++;
+        }
+        cout<<arr[i-1]<<" "<<freq<<endl;
+        i++;
+        freq=1;
+    }
+    if(n==1 || arr[n-1]!=arr[n-2]){
+        cout<<arr[i]<<" "<<freq<<endl;
+    }
+}
+
+int main(){
+    int arr [] ={10,10,20,30,30};
+    int size = sizeof(arr)/sizeof(arr[0]);
+    frequency(arr,size);
+    
+    return 0;
+}
